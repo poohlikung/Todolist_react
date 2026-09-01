93a6f402eb6fbaa163883d76b451508144b546f9
PoohSorawit <hillingstudio@gmail.com>
2026-09-02 05:05:39 +0700

    core:initial

---
+++ todolist/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState,useEffect } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [todos,setTodos] = useState([])
+  useEffect(()=>{
+
+  },[])
+
+
+  return (
+    <>
+     todos.map((todo)=>())
+    </>
+  )
+}
+
+export default App
